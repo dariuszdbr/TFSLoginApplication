89370be073cf1252ef177378224ba42a668846e5
Dariusz <dariusz.dbr@gmail.com>
2017-05-16 21:07:38 +0200

    Added: SqlManager to manage SqlConnections

--- LoginPanelApplication/Properties/Settings.Designer.cs
+++ LoginPanelApplication/Properties/Settings.Designer.cs
@@ -27,7 +27,8 @@ namespace LoginPanelApplication.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Daiusz\\Desktop\\Repo\\" +
-            "LoginPanelApplication\\SqlUserDatabase.mdf;Integrated Security = True")]
+            "TFSLoginApplicationWithLINQ\\LoginPanelApplication\\SqlUserDatabase.mdf;Integrated" +
+            " Security=True")]
         public string connectionString {
             get {
                 return ((string)(this["connectionString"]));
